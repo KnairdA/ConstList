814d2646d5fb2ea0b4534d33da9e6a9af5e235ac
Adrian Kummerlaender <knairda@t-online.de>
2014-11-12 20:06:26 +0100

    Added basic test case for `make` result types * fixed error for lists with length 2 detected through this test case

--- src/make.h
+++ src/make.h
@@ -14,22 +14,22 @@ constexpr auto make(const CAR& car) {
 template <
 	typename CAR,
 	typename CDR,
-	detail::enable_if<!is_cons<CDR>::value> = 0
+	detail::enable_if<is_cons<CDR>::value> = 0
 >
 constexpr auto make(const CAR& car, const CDR& cdr) {
-	return Cons<CAR, decltype(make(cdr))>(
-		car,
-		make(cdr)
-	);
+	return Cons<CAR, CDR>(car, cdr);
 }
 
 template <
 	typename CAR,
 	typename CDR,
-	detail::enable_if<is_cons<CDR>::value> = 0
+	detail::enable_if<!is_cons<CDR>::value> = 0
 >
 constexpr auto make(const CAR& car, const CDR& cdr) {
-	return Cons<CAR, CDR>(car, cdr);
+	return make(
+		car,
+		make<typename detail::replace_type<CAR, CDR>::type>(cdr)
+	);
 }
 
 template <
--- test.cc
+++ test.cc
@@ -1,11 +1,65 @@
+#include "gtest/gtest.h"
+
 #include <cstdint>
 
 #include "list.h"
 
-int main(int, char**) {
-	auto list = ConstList::make<std::uint8_t>(42, 43, 44, 45, 46);
+class ConstListTest : public ::testing::Test {
+	public:
+		template <typename Type>
+		using expected_type = ConstList::Cons<
+			Type,
+			ConstList::Cons<
+				Type,
+				ConstList::Cons<
+					Type,
+					ConstList::Cons<
+						Type,
+						ConstList::Cons<
+							Type,
+							void
+						>
+					>
+				>
+			>
+		>;
 
-	return ConstList::nth<2>(
-		ConstList::take<3>(list)
-	);
+};
+
+TEST_F(ConstListTest, Make) {
+	ASSERT_TRUE((std::is_same<
+		decltype(ConstList::make(42)),
+		ConstList::Cons<int, void>
+	>::value));
+
+	ASSERT_TRUE((std::is_same<
+		decltype(ConstList::make(42, 43)),
+		ConstList::Cons<int, ConstList::Cons<int, void>>
+	>::value));
+
+	ASSERT_TRUE((std::is_same<
+		decltype(ConstList::make<std::uint64_t>(42)),
+		ConstList::Cons<std::uint64_t, void>
+	>::value));
+
+	ASSERT_TRUE((std::is_same<
+		decltype(ConstList::make<std::uint64_t>(42, 43)),
+		ConstList::Cons<std::uint64_t, ConstList::Cons<std::uint64_t, void>>
+	>::value));
+
+	ASSERT_TRUE((std::is_same<
+		decltype(ConstList::make(42, 43, 44, 45, 46)),
+		expected_type<int>
+	>::value));
+
+	ASSERT_TRUE((std::is_same<
+		decltype(ConstList::make<std::uint8_t>(42, 43, 44, 45, 46)),
+		expected_type<std::uint8_t>
+	>::value));
+}
+
+int main(int argc, char **argv) {
+	testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
 }
